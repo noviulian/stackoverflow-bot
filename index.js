9de07e8b65579f82a7258018b68f7fcfe549a7d4
iuliannov <contact@iuliannov.com>
2023-02-05 18:37:08 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { Client, Events, EmbedBuilder } = require("discord.js");
+const axios = require("axios");
+
 require("dotenv").config();
 const cron = require("node-cron");
 // Create a new client instance
@@ -17,6 +19,17 @@ const client = new Client({
 const token = process.env.discordToken;
 const channelId = process.env.discordChannelId;
 
+const stackexchangeTags = [
+  "moralis",
+  "nft",
+  "erc-20",
+  "erc-1155",
+  "erc-721",
+  "auth",
+  "events",
+];
+const stackoverflowTags = ["moralis", "nft", "erc20", "erc721", "erc1155"];
+
 client.once(Events.ClientReady, async (client) => {
   console.log(`Ready! Logged in as ${client.user.tag}`);
   const channel = client.channels.cache.get(channelId);
@@ -27,6 +40,9 @@ client.once(Events.ClientReady, async (client) => {
     .setColor(0x00ae86)
     .setTimestamp();
   await channel.send({ embeds: [onlineEmbed] });
+
+  // get initial questions
+  await getInitialQuestions(channel);
 
   const task = cron.schedule("*/30 * * * *", async () => {
     console.log("task triggered");
@@ -68,7 +84,6 @@ client.once(Events.ClientReady, async (client) => {
     );
 
     if (uniqueQuestions.length > 0) {
-      console.log(uniqueQuestions);
       for (let i = 0; i < uniqueQuestions.length; i++) {
         const question = uniqueQuestions[i];
         const title = question.title;
@@ -89,16 +104,6 @@ client.once(Events.ClientReady, async (client) => {
       }
     }
   });
-
-  const getQuestions = async (time, tag, site) => {
-    const api = `https://api.stackexchange.com/2.3/questions?fromdate=${time}&order=desc&sort=creation&tagged=${tag}&site=${site}`;
-    const res = await axios.get(api);
-    const data = res.data.items;
-    return data;
-  };
-
-  const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
   //start the task
   task.start();
 });
@@ -106,3 +111,71 @@ client.once(Events.ClientReady, async (client) => {
 client.login(token).then(() => {
   console.log("logged in");
 });
+
+const getQuestions = async (time, tag, site) => {
+  const api = `https://api.stackexchange.com/2.3/questions?fromdate=${time}&order=desc&sort=creation&tagged=${tag}&site=${site}`;
+  const res = await axios.get(api);
+  const data = res.data.items;
+  return data;
+};
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function getInitialQuestions(channel) {
+  const current = Math.floor(new Date().getTime() / 1000);
+  const pastTime = current - 60 * 30; // 60 seconds * 30 minutes
+  console.log(pastTime);
+  let questions = [];
+
+  try {
+    for (let index = 0; index < stackexchangeTags.length; index++) {
+      const tag = stackexchangeTags[index];
+      const data = await getQuestions(pastTime, tag, "ethereum");
+      if (data) questions.push(...data);
+      // wait 60 seconds to not get rate-limited
+      await wait(60000);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    for (let index = 0; index < stackoverflowTags.length; index++) {
+      const tag = stackoverflowTags[index];
+      const data = await getQuestions(pastTime, tag, "stackoverflow");
+      if (data) questions.push(...data);
+      // wait 60 seconds to not get rate-limited
+      await wait(60000);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  // filter questions with the same id,
+  let uniqueQuestions = questions.filter(
+    (obj, index) =>
+      questions.findIndex((item) => item.question_id === obj.question_id) ===
+      index
+  );
+
+  if (uniqueQuestions.length > 0) {
+    for (let i = 0; i < uniqueQuestions.length; i++) {
+      const question = uniqueQuestions[i];
+      const title = question.title;
+      const link = question.link;
+      const tags = question.tags;
+
+      const embed = new EmbedBuilder()
+        .setTitle(title)
+        .setURL(link)
+        .setDescription(tags.join(", "))
+        .setColor(0x00ae86)
+        .setTimestamp();
+
+      await channel.send({ embeds: [embed] });
+
+      // wait 5 seconds
+      await wait(5000);
+    }
+  }
+}
